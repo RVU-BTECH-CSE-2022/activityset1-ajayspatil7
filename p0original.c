5969ca2b9d0b79bd06db2088d1bdef1447cc11dc
AJAY S P <51265917+ajayspatil7@users.noreply.github.com>
2022-07-28 11:55:01 +0000

    new prog added

--- p0original.c
+++ p0original.c
@@ -1,1 +1,10 @@
- 
+#include<stdio.h>
+
+int main(){
+
+  float length;
+  float breadth;
+  float area = length*breadth;
+  printf("The area of rectangle with length %f and breadth %f is %f\n", length, breadth, area);
+  return 0;
+}  
